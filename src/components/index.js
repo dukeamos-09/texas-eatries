06e2459892f0770b2c1a3f847d63a61e9fc04676
dukeamos-09 <duke.amos@yahoo.com>
2023-06-18 05:37:28 -0700

    first commit - committing all files to the repository

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+
+export { default as Navbar } from './Navbar/Navbar';     
+export { default as Header } from './Header/Header';     
+export { default as SpecialMenu } from './SpecialMenu/SpecialMenu';     
+export { default as SubHeading } from './SubHeading/SubHeading';     
+export { default as MenuItems } from './MenuItems/MenuItems';     
---
+++ src/constant/data.js
@@ -0,0 +1,36 @@
+import { data } from '.';
+import images from './images';
+
+const meal = [
+  {
+    title: "Toasted Bread Blackberries",
+    price: "$3",
+  },
+
+  {
+    title: "Burger",
+    price: "$1",
+  },
+
+  {
+    title: "Raspberries Cake",
+    price: "$2",
+  },
+
+  {
+    title: "Salad",
+    price: "$5",
+  },
+
+  {
+    title: "Meat Balls",
+    price: "$7",
+  },
+
+  {
+    title: "Grilled Chicken",
+    price: "$7",
+  },
+];
+
+export default data;
